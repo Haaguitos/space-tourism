da02b15659f9556600351b4d9e58aa28b9dba76e
Ígor Haag <igorhaagrod@gmail.com>
2023-07-14 17:50:37 -0300

    fix: remove unused images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-    images: {
-      unoptimized: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
